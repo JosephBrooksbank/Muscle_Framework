71c8b904626088563daffcbaa66a00df95fa82a8
brookshank <jbrooksbank17@moundsparkacademy.org>
2017-02-06 22:57:49 -0600

    more work on comparatives

--- WindowsGame1/WindowsGame1/Scripts/JointComparatives.cs
+++ WindowsGame1/WindowsGame1/Scripts/JointComparatives.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
+﻿using System.Linq;
 using Microsoft.Kinect;
 using Microsoft.Xna.Framework;
 
-namespace KinectDemo.Scripts
-{
-    class JointComparatives : MyGame {
+namespace KinectDemo.Scripts {
+    internal class JointComparatives : MyGame {
+        private JointType altJoint;
         private JointType mainJoint;
         private JointType[] otherJoints;
-        private JointType altJoint;
+
         public void GetJointInfo(JointType joint1, params JointType[] otherjoints) {
             mainJoint = joint1;
             otherJoints = otherjoints;
@@ -20,23 +16,20 @@ namespace KinectDemo.Scripts
         public void GetJointInfo(JointType joint1, JointType joint2) {
             mainJoint = joint1;
             altJoint = joint2;
-
         }
 
         public bool IsHighest() {
-            bool highest = true;
-            foreach (JointType t in otherJoints) {
+            var highest = true;
+            foreach (var t in otherJoints)
                 if (GetJointPosition(mainJoint, ScreenSpace.Screen).Y < GetJointPosition(t, ScreenSpace.Screen).Y)
-                   highest = false;
-            }
+                    highest = false;
             return highest;
         }
 
 
-        public static Vector3 GetJointPosition(JointType joint, MyGame.ScreenSpace type, MyGame.CustomSkeleton skeleton = null)
-        {
-           // if (Instance == null)
-           //     return Vector3.Zero;
+        public static Vector3 GetJointPosition(JointType joint, ScreenSpace type, CustomSkeleton skeleton = null) {
+            // if (Instance == null)
+            //     return Vector3.Zero;
 
             // If the skeleton provided is null then grab the first available skeleton and use it:
             if (skeleton == null && Instance.Skeletons != null && Instance.Skeletons.Count > 0)
@@ -46,9 +39,9 @@ namespace KinectDemo.Scripts
             else
                 return Vector3.Zero;
 
-            if (type == MyGame.ScreenSpace.Screen)
-                return skeleton?.ScaleTo(joint, MyGame.Screen.Width, MyGame.Screen.Height) ?? Vector3.Zero;
-            return skeleton?.ScaleTo(joint, MyGame.World.Width, MyGame.World.Height) ?? Vector3.Zero;
+            if (type == ScreenSpace.Screen)
+                return skeleton?.ScaleTo(joint, Screen.Width, Screen.Height) ?? Vector3.Zero;
+            return skeleton?.ScaleTo(joint, World.Width, World.Height) ?? Vector3.Zero;
         }
     }
 }
